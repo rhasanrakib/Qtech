50d41ca6aef577224f86e9790eae9b4e89c33e8b
Rakib <rakibulh170@gmail.com>
2021-03-26 23:07:28 +0600

    password field ok

--- QtechApps/forms.py
+++ QtechApps/forms.py
@@ -4,19 +4,21 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
+    '''
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-
+    '''
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={ 'class': 'form-control form-control-user', 'placeholder': 'Must be 8 Characters'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={ 'class': 'form-control form-control-user', 'placeholder': 'Confirm Password'}))
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
 
         widgets = {
                 'username': forms.TextInput(attrs={'class': 'form-control required', 'Placeholder': 'Input user name'}),
-                'first_name': forms.TextInput(attrs={'class': 'form-control required', 'Placeholder': 'Input user name'}),
+                'first_name': forms.TextInput(attrs={'class': 'form-control', 'Placeholder': 'Input user name'}),
                 'last_name': forms.TextInput(attrs={'class': 'form-control', 'Placeholder': 'Input your last name'}),
                 'email': forms.EmailInput(attrs={'class': 'form-control required', 'Placeholder': 'Input your email'}),
-                'password1':forms.PasswordInput(attrs={'class': 'form-control required', 'Placeholder': 'Password'}),       
-                'password2':forms.PasswordInput(attrs={'class': 'form-control required', 'Placeholder': 'Confirm Password'})       
+                
         }
